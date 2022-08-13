03e36e64c82aef2128bad6f8115e57ee7ac8cc0e
RINKI NAG <rinkinag24@gmail.com>
2022-08-14 01:12:10 +0530

    Update Binary_search_tree.py

--- BST/Binary_search_tree.py
+++ BST/Binary_search_tree.py
@@ -91,3 +91,20 @@ class BinarySearchTree():
             print(this_node.key, ',' ,end=' ')
             self.preorder(this_node.left)
             self.preorder(this_node.right)
+            
+def invertBinaryTree(tree):
+    if tree is None:
+        return 
+    swapLeftAndRight(tree)
+    invertBinaryTree(tree.left)
+    invertBinaryTree(tree.right)
+
+def swapLeftAndRight(tree):
+    tree.left, tree.right =  tree.right, tree.left
+    
+# This is the class of the input binary tree.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
