8a8d1ee870aa58c733522d3b533950cb58b54192
RINKI NAG <rinkinag24@gmail.com>
2022-08-13 20:54:20 +0530

    Update Binary_search_tree.py

--- BST/Binary_search_tree.py
+++ BST/Binary_search_tree.py
@@ -1,66 +1,64 @@
 class Node:
-	def __init__(self, key):
-		self.right = None
-		self.left = None
-		self.key = key
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.key = key
+
 
 class BinarySearchTree():
-	def __init__(self, root = None):
-		self.root = root
-		
-	def get_root(self):
-		return self.root
-	
-# Insert a node 
-def insert(self, key):
-	if self.root is None:
-		self.root = Node(key)
-	else:
-		self.insertNode(self.root, key)
-		
-def insertNode(self, this_node, key):
-	if self.this_node > key:
-		if self.this_node.left is None:
-			self.this_node.left = Node(key)
-		else:
-			self.insertNode(this_node.left, key)
-	else:
-		if self.this_node.right is None:
-			self.this_node.right = Node(key)
-		else:
-			self.insertNode(this_node.right, key)
-			
-def find_inorder_successor(self, this_node):
-	myval = this_node
-	if myval.left  is not None:
-		myval = myval.left
-	return myval
+    def __init__(self, root=None):
+        self.root = root
 
-def find_inorder_predecssor(self, this_node):
-	myval = this_node
-	if myval.right  is not None:
-		myval = myval.right
-	return myval
-	
-def delNode(self, this_node, key):
-	if this_node is None:
-		reurn this_node
-	if key < this_node.key:
-		this_node.left =  self.delNode(this_node.left, key)
-	if key > this_node.key:
-		this_node.right = self.delNode(this_node.right, key)
-	else:
-		# case 1 or 0 child
-		if this_node.right is None:
-			temp = this_node.left
-			this_node.left = None
-			return temp
-		elif this_node.left is None:
-			temp = this_node.right
-			this_node.right = None
-			return temp
-		# case 2 children
-		temp = self.find_inorder_successor(self, this_node.right)
-		this_node.key = temp
-		this_node.right = self.delNode(this_node.right , temp)
-	return this_node
+    def get_root(self):
+        return self.root
+
+    def insert(self, key):
+        if self.root is None:
+            self.root = Node(key)
+        else:
+            self.insert_helper(self.root, key)
+
+    def insert_helper(self, this_node, key):
+        if this_node.key > key:
+            if this_node.left is None:
+                this_node.left = Node(key)
+            else:
+                self.insert_helper(this_node.left, key)
+
+        else:
+            if this_node.right is None:
+                this_node.right = Node(key)
+            else:
+                self.insert_helper(this_node.right, key)
+
+
+    def find_inorder_successor(self, this_node):
+        myval = this_node
+        while myval.left is not None:
+            myval = myval.left
+        return myval
+
+    def delete_node(self, this_node, key):
+        if this_node is None:
+            return this_node
+        if key < this_node.key:
+            this_node.left = self.delete_node(this_node.left, key)
+        elif key > this_node.key:
+            this_node.right = self.delete_node(this_node.right, key)
+        else:
+            # case with no child or 1 child
+            if this_node.left is None:
+                temp = this_node.right
+                this_node = None
+                return temp
+            elif this_node.right is None:
+                temp = this_node.left
+                this_node = None
+                return temp
+
+            # case with 2 childen
+            temp = self.find_inorder_successor(this_node.right)
+
+            this_node.key = temp.key
+            this_node.right = self.delete_node(this_node.right, temp.key)
+        return this_node
