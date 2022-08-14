2f3f9437796706a500a856ebb483fe5caf3fb6ae
RINKI NAG <rinkinag24@gmail.com>
2022-08-14 23:29:12 +0530

    Create DFS.py

---
+++ Graph/DFS.py
@@ -0,0 +1,34 @@
+# DFS
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        array.append(self.name)
+        for child in self.children:
+            child.depthFirstSearch(array)
+        return array
+	
+	# BFS
+	class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+	
+	def breadthFirstSearch(self, array):
+		queue = [self]
+		while len(queue) > 0:
+			current = queue.pop(0)
+			array.append(current.name)
+			for child in current.children:
+				queue.append(child)
+		return array
